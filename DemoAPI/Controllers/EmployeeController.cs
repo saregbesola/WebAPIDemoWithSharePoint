110a0a4a37a875b08dad24c45ddbe591f1eff879
Sunday Aregbesola <s.aregbesola@gmail.com>
2018-01-02 10:57:15 -0500

    importing project

---
+++ DemoAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemoAPI.Models;
+
+namespace DemoAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using (StaffDemoDBEntities entities = new StaffDemoDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
